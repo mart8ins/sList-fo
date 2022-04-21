ae212133344fc8679dd40e79be2fdf212517feec
mart8ins <mart8ins@gmail.com>
2022-04-21 16:00:27 +0300

    added functionality to send recipe groceries to new shopping list if there is no existing

--- src/components/recipes/components/RecipeDetails.tsx
+++ src/components/recipes/components/RecipeDetails.tsx
@@ -6,6 +6,7 @@ import { Recipe } from "../../../models/models";
 import CloseModalButton from "../../landing/components/components/shared/closeModalButton/CloseModalButton";
 import SingleListItem from "../../landing/components/components/shared/singleListItem/SingleListItem";
 import "./recipeDetails.css";
+import { v4 as uuidv4 } from "uuid";
 
 type Props = {
     recipeId: string;
@@ -30,6 +31,8 @@ function RecipeDetails({ recipeId }: Props) {
         setChoosenRecipeIDWhereToSendGroceries,
     ] = useState("");
 
+    const [newListTitle, setNewListTitle] = useState("");
+
     useEffect(() => {
         const filtered = recipes.filter((recipe) => {
             return recipe.id === recipeId;
@@ -38,6 +41,7 @@ function RecipeDetails({ recipeId }: Props) {
     }, [recipeId, recipes]);
 
     const sendRecipeGroceriesToShoppingList = () => {
+        const groceries = recipeDetails.recipeGroceriesList;
         // OPEN OPTION TO CHOOSE SHOPPING LIST WHERE TO SEND GROCERIES
         if (!showRecipeNamesWhereToSendGroceries) {
             setShowRecipeNameWhereToSendGroceries(true);
@@ -49,8 +53,6 @@ function RecipeDetails({ recipeId }: Props) {
             choosenRecipeIDWhereToSendGroceries
         ) {
             const id = choosenRecipeIDWhereToSendGroceries;
-            const groceries = recipeDetails.recipeGroceriesList;
-
             const shoppingListsRef = [...shoppingLists];
             shoppingListsRef.forEach((item) => {
                 if (item.id === id) {
@@ -64,6 +66,26 @@ function RecipeDetails({ recipeId }: Props) {
             setShowRecipeNameWhereToSendGroceries(false);
             setChoosenRecipeIDWhereToSendGroceries("");
         }
+        if (showRecipeNamesWhereToSendGroceries && newListTitle.length > 0) {
+            const newSList = {
+                authorId: uuidv4(),
+                id: uuidv4(),
+                title: newListTitle,
+                groceries,
+                completed: false,
+            };
+            updateShoppingLists([newSList]);
+            setShowRecipeNameWhereToSendGroceries(false);
+            setNewListTitle("");
+        }
+    };
+
+    // const createNewShoppingListIfNoExists = () => {
+    //     console.log("jauns shop lists");
+    // };
+
+    const handleNewListTitle = (e: any) => {
+        setNewListTitle(e.target.value);
     };
 
     return (
@@ -72,9 +94,11 @@ function RecipeDetails({ recipeId }: Props) {
             <div className="option__buttons">
                 <button onClick={sendRecipeGroceriesToShoppingList}>
                     {!choosenRecipeIDWhereToSendGroceries &&
+                        newListTitle.length === 0 &&
                         "Choose shopping list"}
-                    {showRecipeNamesWhereToSendGroceries &&
-                        choosenRecipeIDWhereToSendGroceries &&
+                    {((showRecipeNamesWhereToSendGroceries &&
+                        choosenRecipeIDWhereToSendGroceries) ||
+                        newListTitle.length > 0) &&
                         "Send groceries to shopping list"}
                 </button>
                 <button
@@ -90,24 +114,41 @@ function RecipeDetails({ recipeId }: Props) {
 
             {showRecipeNamesWhereToSendGroceries && (
                 <div className="recipe__for__choose">
-                    {shoppingListNamesForChooseOption.map((item) => {
-                        return (
-                            <div
-                                key={item.listId}
-                                className={`${
-                                    choosenRecipeIDWhereToSendGroceries ===
-                                        item.listId && "selected__recipe"
-                                }`}
-                                onClick={() =>
-                                    setChoosenRecipeIDWhereToSendGroceries(
-                                        item.listId
-                                    )
-                                }
-                            >
-                                {item.listTitle}
-                            </div>
-                        );
-                    })}
+                    {shoppingListNamesForChooseOption.length > 0 ? (
+                        shoppingListNamesForChooseOption.map((item) => {
+                            return (
+                                <div
+                                    key={item.listId}
+                                    className={`choose__recipe ${
+                                        choosenRecipeIDWhereToSendGroceries ===
+                                            item.listId && "selected__recipe"
+                                    }`}
+                                    onClick={() =>
+                                        setChoosenRecipeIDWhereToSendGroceries(
+                                            item.listId
+                                        )
+                                    }
+                                >
+                                    {item.listTitle}
+                                </div>
+                            );
+                        })
+                    ) : (
+                        <div className="new__recipe">
+                            <label>
+                                Send to new shopping list
+                                <input
+                                    type="text"
+                                    placeholder="Enter lists title"
+                                    onChange={handleNewListTitle}
+                                    value={newListTitle}
+                                />
+                            </label>
+                            {/* <span onClick={createNewShoppingListIfNoExists}>
+                                Create
+                            </span> */}
+                        </div>
+                    )}
                 </div>
             )}
 
